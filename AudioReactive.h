e41013f85295a17004d87a66a472bbd80fb7ed09
KNOCKS <gtamayo36@gmail.com>
2026-03-23 19:09:50 -0700

    Update AudioReactive.h

--- AudioReactive.h
+++ AudioReactive.h
@@ -1,14 +1,30 @@
-#ifndef KNOCKS_AUDIO_H
-#define KNOCKS_AUDIO_H
+/* * KNOCKSSTUDiOS ™ | ORIGINAL 9K ULTRA POWER
+ * MODULE: SENTINEL V SENSORY ARRAY (MAD MODE)
+ */
+
+#ifndef AUDIO_REACTIVE_H
+#define AUDIO_REACTIVE_H
 
 namespace KnocksEngine {
     class AudioShine {
     public:
         float intensity = 1.0f;
-        // This detects voice levels to make the 9K metal pop
+
+        // Detects raspy Sentinel voice to trigger Cyan Glow
         void ReactToVoice(float decibels) {
             intensity = 1.0f + (decibels / 100.0f);
+            
+            if (decibels > 90.0f) {
+                // COMMAND: IGNITE STEALTH V ON CHEST PLATE
+                this->IgniteMadMode();
+            }
+        }
+
+    private:
+        void IgniteMadMode() {
+            // SETS EMISSIVE TO CYAN (THROUGH OBSIDIAN ARMOR)
         }
     };
 }
+
 #endif
