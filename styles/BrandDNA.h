5a6ec0f7a46ad93a003c699a31e49f9b5f821f16
KNOCKS <gtamayo36@gmail.com>
2026-03-23 17:52:26 -0700

    Create BrandDNA.h

---
+++ styles/BrandDNA.h
@@ -0,0 +1,18 @@
+#ifndef KNOCKS_BRAND_DNA_H
+#define KNOCKS_BRAND_DNA_H
+
+namespace KnocksEngine {
+    struct ColorHex { const char* hex; const char* name; };
+
+    // THE 9K JUNGLE PALETTE ™
+    ColorHex NEON_LIME   = {"#32CD32", "Acid Jungle Green"};
+    ColorHex ELECTRIC_CYAN = {"#00FFFF", "Deep Sea Blue"};
+    ColorHex CANDY_RED   = {"#FF0800", "Volcanic Crimson"};
+    ColorHex SUN_YELLOW  = {"#FFD700", "9K Gold Glow"};
+
+    // Function to "Print" the logo style
+    void GenerateLogoStyle() {
+        // This is the instruction for your 9K Logo Creator
+    }
+}
+#endif
